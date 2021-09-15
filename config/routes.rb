403955942ba951059377d75a0fc820b55d8f899f
geefuoco <gianluca_1227@hotmail.com>
2021-09-15 14:28:55 -0400

    Added nested comments resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :comments do
+    resources :comments
+  end
+
   devise_for :users, controllers: { registrations: "registrations"}
 
   authenticated do
